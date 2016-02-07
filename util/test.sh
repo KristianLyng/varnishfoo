242303024eb7f8df008f5d007dd0f9b1b806c214
Kristian Lyngstol <kristian@bohemians.org>
2016-02-07 02:59:14 +0100

    utils: Improve test tool

--- util/test.sh
+++ util/test.sh
@@ -2,7 +2,7 @@
 
 VARNISHD=/usr/local/sbin/varnishd
 TARGET=$(mktemp -d /tmp/foo.XXXXXX)
-cat chapter-4.rst | awk -v target=${TARGET} '
+awk -v target=${TARGET} '
 /.. code::/ {
 	n += 1
 	code=1
@@ -18,7 +18,7 @@ cat chapter-4.rst | awk -v target=${TARGET} '
 END {
 	print n " VCLs written to " target
 }
-'
+' "$@"
 
 testvcl(){
 	VCL=$1
@@ -26,14 +26,24 @@ testvcl(){
 	OUT=$(${VARNISHD} -n ${TARGET} -C -f ${VCL} 2>&1)
 	if [ $? -eq "0" ]; then
 		echo "FINE!";
+		return 0
 	else
 		echo -e "$OUT"
+		return 1
 	fi
 }
 
+_tmp=${TARGET}'/*.vcl'
 
+if [ $_tmp = "$_tmp" ]; then
+	echo "Nothing to test"
+	rm -r ${TARGET}
+	exit 0
+fi
+ret=0
 for a in ${TARGET}/*.vcl; do
 	testvcl $a
+	ret=$(( $ret + $? ))
 done
-
-
+rm -r ${TARGET}
+exit $ret
